6f991446b3b538e262c68e53d229d41dfe7a00c5
rafi alariq <rmalariq@gmail.com>
2023-05-22 12:03:17 +0700

    initial commit

---
+++ model/user.go
@@ -0,0 +1,12 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	PhoneNumber string `json:"phone_number"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+}
